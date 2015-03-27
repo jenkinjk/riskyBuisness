ee46a8fb407a0378925da844ef40e46d25b595ac
jenkinjk <jenkinjk@rose-hulman.edu>
2015-03-27 08:30:06 -0400

    Redid commits. Some weird git hub thing.

---
+++ RiskyBuisness/src/ContinentListener.java
@@ -0,0 +1,20 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ContinentListener implements ActionListener {
+	private String label;
+
+	public ContinentListener(String label) {
+		this.label = label;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		System.out.printf(identity());
+
+	}
+
+	public String identity() {
+		return String.format("I am %s. Fear me!\n", label);
+	}
+}
