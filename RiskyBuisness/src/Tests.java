281ad76216046a657b394bbeba33b50c807509ce
jenkinjk <jenkinjk@rose-hulman.edu>
2015-04-15 13:46:21 -0400

    Parameterized the tests
    It works with any number of players (1-6)

--- RiskyBuisness/src/Tests.java
+++ RiskyBuisness/src/Tests.java
@@ -2,9 +2,94 @@ import static org.junit.Assert.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import org.junit.Test;
+import java.util.Arrays;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(org.junit.runners.Parameterized.class)
 public class Tests {
+	private int fInput;
+
+	@Parameters
+	public static Iterable<Object[]> data() {
+		return Arrays.asList(new Object[][] { { 1 }, { 2 }, { 3 }, { 4 },
+				{ 5 }, { 6 } });
+	}
+
+	public Tests(int input) {
+		fInput = input;
+	}
+
+	@Test
+	public void setupTerritoriesEquallyTest() {
+		RiskBoard board = new RiskBoard();
+		board.initialGame(fInput);
+		ContinentListener c = new ContinentListener("Asia", board);
+		c.setUpTerritories();
+		ArrayList<Player> players = board.getPlayers();
+		assertTrue(playersEquallyDiveded(players));
+	}
+
+	private boolean playersEquallyDiveded(ArrayList<Player> players) {
+		for (Player p : players) {
+			for (Player p2 : players) {
+				if (!(p.getNumberOfTerritories() == p2.getNumberOfTerritories()
+						|| p.getNumberOfTerritories() == p2
+								.getNumberOfTerritories() + 1 || p
+						.getNumberOfTerritories() + 1 == p2
+						.getNumberOfTerritories())) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	// This is unsatisfactorily tested. It only tests if there are exactly 5
+	// players. Should be parameterized.
+
+	@Test
+	public void setupTerritoriesRandomlyTest() {
+		RiskBoard board = new RiskBoard();
+		board.initialGame(fInput);
+		RiskBoard board2 = new RiskBoard();
+		board2.initialGame(fInput);
+		ContinentListener c = new ContinentListener("Asia", board);
+		c.setUpTerritories();
+		ArrayList<Player> players = board.getPlayers();
+		ContinentListener c2 = new ContinentListener("Asia", board2);
+		c2.setUpTerritories();
+		ArrayList<Player> players2 = board2.getPlayers();
+		int count = 0;
+		boolean failing = true;
+		while (failing) {
+			for (int i = 0; i < fInput; i++) {
+				ArrayList<Territory> territories = players.get(i)
+						.getTerritories();
+				ArrayList<Territory> territories2 = players2.get(i)
+						.getTerritories();
+				if (territories.size() > territories2.size()
+						|| territories.size() < territories2.size()) {
+					failing = false;
+				} else {
+					for (int j = 0; j < territories.size(); j++) {
+						if (!territories2.contains(territories.get(j))) {
+							failing = false;
+							break;
+						}
+					}
+				}
+			}
+			if (count >= 100)
+				break;
+			count++;
+		}
+		assertFalse(failing);
+	}
+
+
 
 	@Test
 	public void displayBoardTest() throws IOException {
@@ -45,74 +130,6 @@ public class Tests {
 		Player player5 = new Player("Player Five");
 		assertEquals(player5.getName(), board.getNextPlayer().getName());
 		assertEquals(player1.getName(), board.getNextPlayer().getName());
-	}
-
-	// This is unsatisfactorily tested. It only tests if there are exactly 5
-	// players. Should be generalized and parameterized.
-	@Test
-	public void setupTerritoriesEquallyTest() {
-		RiskBoard board = new RiskBoard();
-		board.initialGame(5);
-		ContinentListener c = new ContinentListener("Asia", board);
-		c.setUpTerritories();
-		ArrayList<Player> players = board.getPlayers();
-		assertTrue(playersEquallyDiveded(players));
-	}
-
-	private boolean playersEquallyDiveded(ArrayList<Player> players) {
-		for (Player p : players) {
-			for (Player p2 : players) {
-				if (!(p.getNumberOfTerritories() == p2.getNumberOfTerritories()
-						|| p.getNumberOfTerritories() == p2
-								.getNumberOfTerritories() + 1 || p
-						.getNumberOfTerritories() + 1 == p2
-						.getNumberOfTerritories())) {
-					return false;
-				}
-			}
-		}
-		return true;
-	}
-
-	// This is unsatisfactorily tested. It only tests if there are exactly 5
-	// players. Should be parameterized.
-	@Test
-	public void setupTerritoriesRandomlyTest() {
-		RiskBoard board = new RiskBoard();
-		board.initialGame(5);
-		RiskBoard board2 = new RiskBoard();
-		board2.initialGame(5);
-		ContinentListener c = new ContinentListener("Asia", board);
-		c.setUpTerritories();
-		ArrayList<Player> players = board.getPlayers();
-		ContinentListener c2 = new ContinentListener("Asia", board2);
-		c2.setUpTerritories();
-		ArrayList<Player> players2 = board2.getPlayers();
-		int count = 0;
-		boolean failing = true;
-		while (failing) {
-			for (int i = 0; i < 5; i++) {
-				ArrayList<Territory> territories = players.get(i)
-						.getTerritories();
-				ArrayList<Territory> territories2 = players2.get(i)
-						.getTerritories();
-				if (territories.size() > territories2.size()
-						|| territories.size() < territories2.size()) {
-					failing = false;
-				} else {
-					for (int j = 0; j < territories.size(); j++) {
-						if (!territories2.contains(territories.get(j))) {
-							failing = false;
-							break;
-						}
-					}
-				}
-			}
-			if (count >= 100)
-				break;
-			count++;
-		}
-		assertFalse(failing);
 	}
 
 }
