6f52baf6384c7a1fc50c7405cbae084d58315cb7
jenkinjk <jenkinjk@rose-hulman.edu>
2015-04-23 11:47:59 -0400

    All pass
    Note, we need to start considering another test file so we can re
    paramaterize

--- RiskyBuisness/src/Battle.java
+++ RiskyBuisness/src/Battle.java
@@ -12,7 +12,10 @@ public class Battle {
 		this.defender = b;
 	}
 
-	public void execute(int attacker, int defender) {
+	public void execute(int attacker, int defender) throws Exception {
+		//if(attacker>this.attacker.size()||defender>this.defender.size()) throw new Exception("You cannot Roll more dice than you have armies.");
+		if(defender!=1&&defender!=2) throw new Exception("The defender can only roll 1 or 2 dice.");
+		if(attacker!=2&&attacker!=1&&attacker!=3) throw new Exception("The attacker can only roll 1, 2 or 3 dice.");
 		this.attacker.takeLosses(defender);
 		this.defender.takeLosses(attacker);
 		
