1ecdeb3dfab42996036006ec0275e8ded74ed754
jenkinjk <jenkinjk@rose-hulman.edu>
2015-04-23 09:47:31 -0400

    All pass

--- RiskyBuisness/src/Battle.java
+++ RiskyBuisness/src/Battle.java
@@ -1,8 +1,8 @@
 
 public class Battle {
 
-	public Battle(Army a, Army b) {
-		// TODO Auto-generated constructor stub
+	public Battle(Army a, Army b) throws Exception {
+		if(a.getOwner().equals(b.getOwner())) throw new Exception("You cannot attack your own armies.");
 	}
 
 }
