045801918bae3251890bf446827870387da359da
jenkinjk <jenkinjk@rose-hulman.edu>
2015-04-23 11:50:38 -0400

    All pass

--- RiskyBuisness/src/Battle.java
+++ RiskyBuisness/src/Battle.java
@@ -13,7 +13,7 @@ public class Battle {
 	}
 
 	public void execute(int attacker, int defender) throws Exception {
-		//if(attacker>this.attacker.size()||defender>this.defender.size()) throw new Exception("You cannot Roll more dice than you have armies.");
+		if(attacker>this.attacker.size()||defender>this.defender.size()) throw new Exception("You cannot Roll more dice than you have armies.");
 		if(defender!=1&&defender!=2) throw new Exception("The defender can only roll 1 or 2 dice.");
 		if(attacker!=2&&attacker!=1&&attacker!=3) throw new Exception("The attacker can only roll 1, 2 or 3 dice.");
 		this.attacker.takeLosses(defender);
