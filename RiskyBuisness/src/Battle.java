b69f401f482a0d4a36a86692081d91a238526a19
jenkinjk <jenkinjk@rose-hulman.edu>
2015-04-23 11:02:49 -0400

    All pass

--- RiskyBuisness/src/Battle.java
+++ RiskyBuisness/src/Battle.java
@@ -3,6 +3,7 @@ public class Battle {
 
 	public Battle(Army a, Army b) throws Exception {
 		if(a.getOwner().equals(b.getOwner())) throw new Exception("You cannot attack your own armies.");
+		if(!(a.getLocation().getNeighbors().contains(b.getLocation()))) throw new Exception("You cannot attack non adjacent territories.");
 	}
 
 }
