1115d37b87b44eaf244eaf650e149ac908fe75e9
Zachary Haloski <zacharyhaloski@yahoo.com>
2015-04-24 13:11:56 -0400

    Passing RandomTest for Army Battles
    Added rolling to Battle class

--- RiskyBuisness/src/Battle.java
+++ RiskyBuisness/src/Battle.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 
 public class Battle {
 
@@ -13,11 +14,33 @@ public class Battle {
 	}
 
 	public void execute(int attacker, int defender) throws Exception {
+		Random gen = new Random();
 		if(attacker>this.attacker.size()||defender>this.defender.size()) throw new Exception("You cannot Roll more dice than you have armies.");
 		if(defender!=1&&defender!=2) throw new Exception("The defender can only roll 1 or 2 dice.");
 		if(attacker!=2&&attacker!=1&&attacker!=3) throw new Exception("The attacker can only roll 1, 2 or 3 dice.");
-		this.attacker.takeLosses(defender);
-		this.defender.takeLosses(attacker);
+		int rounds = 0;
+		int attkRoll = 0;
+		int defRoll = 0;
+		while(rounds < defender) {
+			int attacks = 0;
+			int defends = 0;
+			while(attacks < attacker) {
+				attkRoll = Math.max(attkRoll, gen.nextInt(6) + 1);
+				attacks++;
+			}
+			while(defends < defender) {
+				defRoll = Math.max(defRoll, gen.nextInt(6) + 1);
+				defends++;
+			}
+			if(attkRoll > defRoll) {
+				this.defender.takeLosses(1);
+			} else {
+				this.attacker.takeLosses(1);
+			}
+			attkRoll = 0;
+			defRoll = 0;
+			rounds++;
+		}
 	}
 
 }
