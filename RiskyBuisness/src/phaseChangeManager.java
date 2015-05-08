9d1e29ddcfb5e1d30c7f3667daf698a018d2895d
CHENG ZENG <zengc@rose-hulman.edu>
2015-05-07 20:04:36 -0400

    pass phasechangemanager constructor test

--- RiskyBuisness/src/phaseChangeManager.java
+++ RiskyBuisness/src/phaseChangeManager.java
@@ -2,7 +2,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class PhaseChangeManager implements ActionListener {
+	private RiskBoard board;
 	
+	public PhaseChangeManager(RiskBoard board) {
+		this.board = board;
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
